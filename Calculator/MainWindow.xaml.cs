61c9b6b7328e8892276b208f8d892d4598865a6b
Tim Weis <93980301+tiwe1001@users.noreply.github.com>
2024-03-09 12:12:43 +0100

    added logical operators and functions

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string output = "";
+        string operators = "";
+        double numOne, NumTwo, solution;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,7 +33,101 @@ namespace Calculator
         {
             string name = ((Button)sender).Name;
 
-            MessageBox.Show(name);
+            switch (name)
+            {
+                case "OneBtn":
+                    output += "1";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "TwoBtn":
+                    output += "2";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "ThreeBtn":
+                    output += "3";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "FourBtn":
+                    output += "4";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "FiveBtn":
+                    output += "5";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "SixBtn":
+                    output += "6";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "SevenBtn":
+                    output += "7";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "EightBtn":
+                    output += "8";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "NineBtn":
+                    output += "9";
+                    OutputTextBlock.Text = output;
+                    break;
+                case "ZeroBtn":
+                    output += "0";
+                    OutputTextBlock.Text = output;
+                    break;
+            }
+        }
+
+        private void OperationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            numOne = Double.Parse(output);
+            output = "";
+            string name = ((Button)sender).Name;
+
+            switch (name)
+            {
+                case "DivideBtn":
+                    operators = "divide";
+                    OutputTextBlock.Text = "/";
+                    break;
+                case "MultiplyBtn":
+                    operators = "multiply";
+                    OutputTextBlock.Text = "*";
+                    break;
+                case "SubtractBtn":
+                    operators = "subtract";
+                    OutputTextBlock.Text = "-";
+                    break;
+                case "AddBtn":
+                    operators = "add";
+                    OutputTextBlock.Text = "+";
+                    break;
+            }
+        }
+
+        private void SolutionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NumTwo = Double.Parse(output);
+
+            if (operators == "divide")
+            {
+                solution = numOne / NumTwo;
+            }
+            else if (operators == "multiply")
+            {
+                solution = numOne * NumTwo;
+            }
+            else if (operators == "subtract")
+            {
+                solution = numOne - NumTwo;
+            }
+            else if (operators == "add")
+            {
+                solution = numOne + NumTwo;
+            }
+
+            string finish = solution.ToString();
+            OutputTextBlock.Text = finish;
         }
     }
 }
